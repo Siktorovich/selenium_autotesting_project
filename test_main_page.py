54a4efcb0ad2362fed6fd1f9934aee0ea1efaab6
Siktorovich <artemarhipov99@gmail.com>
2022-08-27 12:22:51 +0300

    Add tests for login page (Checking url, login form, register form)

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,5 @@
-from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
-
+from .pages.login_page import LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
@@ -8,3 +7,6 @@ def test_guest_can_go_to_login_page(browser):
     page.open()
     page.go_to_login_page()
     page.should_be_login_link()
+    new_link = browser.current_url
+    login_page = LoginPage(browser, new_link)
+    login_page.should_be_login_page()
